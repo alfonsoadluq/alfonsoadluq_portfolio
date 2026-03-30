6010ed4cce4fe4bb5b24c52345ee61e590752336
Alfonso Adiego Luque <alfonsoaldev@gmail.com>
2026-03-30 22:16:43 +0200

    Adding projects

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,6 +1,19 @@
 import logoAlfonso from '@img/logo.png';
+
 import GitHubIcon from '@icon/github_dark.svg';
 import TailWindIcon from '@icon/tailwindcss.svg';
+import PhpIcon from '@icon/PHP.svg';
+import JsIcon from '@icon/javascript.svg';
+import CssIcon from '@icon/css.svg';
+import AstroIcon from '@icon/astro-icon-dark.svg';
+import TypescriptIcon from '@icon/typescript.svg';
+import LaravelIcon from '@icon/laravel.svg';
+import cIcon from '@icon/c.svg';
+import CppIcon from '@icon/c-plusplus.svg';
+import WordpressIcon from '@icon/wordpress.svg';
+import ReactIcon from '@icon/react_dark.svg';
+
+import kitkeeperPreview from '@project/kitkeeper.png';
 
 export const Images = {
   logos: {
@@ -9,5 +22,18 @@ export const Images = {
   icons: {
     GitHubIcon: GitHubIcon,
     TailWindIcon: TailWindIcon,
-  }
+    PhpIcon: PhpIcon,
+    JsIcon: JsIcon,
+    CssIcon: CssIcon,
+    AstroIcon: AstroIcon,
+    TypescriptIcon: TypescriptIcon,
+    LaravelIcon: LaravelIcon,
+    cIcon: cIcon,
+    CppIcon: CppIcon,
+    WordpressIcon: WordpressIcon,
+    ReactIcon: ReactIcon,
+  },
+  projects: {
+    KitKeeperPreview: kitkeeperPreview,
+  },
 };
