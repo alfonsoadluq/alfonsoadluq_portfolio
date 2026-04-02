9f3fadbe02f088cd61d4c5cdfad0c6c691815d93
Alfonso Adiego Luque <alfonsoaldev@gmail.com>
2026-04-02 17:27:34 +0200

    Fixing visual bugs

--- src/scripts/about.ts
+++ src/scripts/about.ts
@@ -1,24 +1,19 @@
-// src/scripts/about.ts
-
 export const initAbout = () => {
   const items = document.querySelectorAll(".stack-accordion-item");
   if (items.length === 0) return;
 
-  // UX SENIOR: Abrir el primer elemento por defecto para invitar a la interacción
   items[0].classList.add("is-active");
 
   items.forEach((item) => {
     const trigger = item.querySelector(".accordion-trigger");
 
     trigger?.addEventListener("click", () => {
-      // Lógica Exclusiva: Cerramos todos menos el clickeado
       items.forEach((i) => {
         if (i !== item) {
           i.classList.remove("is-active");
         }
       });
 
-      // Alternamos el estado del actual
       item.classList.toggle("is-active");
     });
   });
